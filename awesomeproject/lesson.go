d666a1fc05f368fdbabc847226cb3bfcfe59620a
島倉健麿 <>
2025-02-17 21:52:47 +0900

    array, slice追加

--- awesomeproject/lesson.go
+++ awesomeproject/lesson.go
@@ -2,34 +2,46 @@ package main
 
 import (
 	"fmt"
-	"os/user"
-	"time"
+	// "os/user"
+	// "time"
 )
 
-func init(){
+func init() {
 	fmt.Println("initialized!")
 }
 
-func bazz(){
+func bazz() {
 	fmt.Println("Bazz")
 }
 
-func main(){
-	bazz()
-	fmt.Println("HELLO WORLD")
-	fmt.Println(time.Now())
-	fmt.Println(user.Current())
+func main() {
+	// bazz()
+	// fmt.Println("HELLO WORLD")
+	// fmt.Println(time.Now())
+	// fmt.Println(user.Current())
 
-	var i int = 1
-	fmt.Println(i)
+	// var i int = 1
+	// fmt.Println(i)
 
-	var t,f bool = true, false
-	fmt.Println(t,f)
+	// var t,f bool = true, false
+	// fmt.Println(t,f)
 
+	// var (
+	// 	j int = 3
+	// 	isActive bool = false
+	// )
+	// fmt.Println(j, isActive)
 
-	var (
-		j int = 3
-		isActive bool = false
-	)
-	fmt.Println(j, isActive)
+	var a [2]int
+	fmt.Println(a)
+
+	var s []int
+	s = append(s, 1)
+	fmt.Println(s)
+
+	ss := []int{1, 2, 3}
+	fmt.Println(ss)
+
+	m := map[string]int{"apple": 100}
+	fmt.Println((m))
 }
